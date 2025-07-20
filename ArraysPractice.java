8fa382eb68e84b63f60c87102af6de8f76e30e16
voidyounas <younaskhan120307@gmail.com>
2025-07-20 23:59:25 +0500

    Sum of two arrays and storing in 3rd array

---
+++ ArraysPractice.java
@@ -0,0 +1,31 @@
+public class ArraysPractice{
+    public static void main(String[] args) {
+        
+        int [] arr = {4 , 5 , 5 , 6 , 7};
+        int [] arr2 = {4 , 2 , 7 , 9, 3};
+
+        int result [] = new int[arr.length];
+
+
+        for(int i  =0; i<arr.length; i++){
+            result [i] = arr[i] + arr2[i];  
+        }
+
+        System.out.println("The result array is : ");
+        for(int i =0; i<result.length; i++){
+            System.out.println(result[i] + "  ");
+        }
+
+       
+
+        
+
+        
+
+        
+
+    
+
+    
+    }
+}
