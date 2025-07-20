5668a5926939627f0de89ba9c794b77ffcaf03e2
voidyounas <younaskhan120307@gmail.com>
2025-07-21 02:26:28 +0500

    written after 1 hr of debugging and F-around

--- _linearSearch.java
+++ _linearSearch.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class _linearSearch{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[5];
+        int arr[] = new int[sc.nextInt()];
         System.out.println("Enter elements of array  ");
         for(int i =0; i<arr.length;i++){
             arr[i]= sc.nextInt();
