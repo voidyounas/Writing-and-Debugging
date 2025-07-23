4bc44d695db290d7be13d4152659a480ef36f3ea
voidyounas <younaskhan120307@gmail.com>
2025-07-23 07:47:22 +0500

    Ft : minor bug fix at #19

--- Fib.java
+++ Fib.java
@@ -16,7 +16,7 @@ public class Fib {
         int first =0;
         int second =1;
 
-        for(int i =3; i<=n; i++){
+        for(int i =2; i<=n; i++){
             next = first + second ;
             first = second;
             second = next;
