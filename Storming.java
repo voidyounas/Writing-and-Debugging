540cc4a45109e0d630b73d238a1ace746fec858e
voidyounas <younaskhan120307@gmail.com>
2025-07-24 08:59:26 +0500

    Almost all  string functions enough for you, just read and execute

--- Storming.java
+++ Storming.java
@@ -24,19 +24,32 @@ public class Storming{
         System.out.println(index);//gives the last index where a is occured 
 
         System.out.println(str.equalsIgnoreCase(Str2));//comparing both strings 
+
         System.out.println(str.charAt(2));//this will give you the letter at index 3 
+
         System.out.println(str.compareTo(abc));//this will compare the strings in lexicographical order will give the result in int like (0 for equals) (- for less than ) (+ for greater than )
 
-        System.out.println(str.startsWith("Younas"));//check if the strinng starts with the given string  
+
+        System.out.println(str.startsWith("Younas"));//check if the strinng starts with the given string 
+
         System.out.println(str.endsWith("khan"));//check if the string ends with the provided string or something like that 
+
         System.out.println(str.contains("oun"));//check for the string if it is present in the first one or not 
+
         System.out.println(str.substring(0,6));//will give you the sub string from 0 index to 5th index 
+
         System.out.println(str.substring(7));//it will give you the sub string from index 7 to the end 
+
         System.out.println(str.replace('a', 'b'));//this will replace all the a with b 
+
         System.out.println(Arrays.toString(str.split(" ")));//this will split the string on the basis of spaces to the array **must to learn for file handling**
+
         System.out.println(str.trim());//this will remove the ending and starting spaces 
+
         System.out.println(str.isEmpty());//this check if the string is empty or not 
+
         System.out.println(str.isBlank());//this will check is the string is empty or contain spaces 
+        
 
         //There are many more but these are the usefull and you can easily work  with these just have grip on them and use it in your codes and projects they are really helpfull 
     }
